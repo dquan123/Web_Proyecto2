10638dce42791828653987dcd4dfe555f62c72bc
Diego Quan <dquan@delicatessen502.com>
2026-05-15 19:31:30 -0600

    Módulo que renderiza el formulario de estampas

---
+++ frontend/src/components/ListaItem.jsx
@@ -0,0 +1,24 @@
+import itemCard from "./ItemCard";
+
+function ListaItem(items, onArchivar, onEditar) {
+    const itemArchivos = items.filter(item => item.activo)
+    
+    if (itemArchivos.length === 0) {
+        return <p>No hay estampas aún</p>
+    }
+
+    return (
+        <div>
+            {itemArchivos.map(item => (
+                <itemCard 
+                    key={item.id} 
+                    item={item} 
+                    onArchivar={onArchivar} 
+                    onEditar={onEditar} 
+                />
+            ))}
+        </div>
+    )
+}
+
+export default ListaItem

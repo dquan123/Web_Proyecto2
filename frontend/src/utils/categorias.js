6ab1945acc69289bce1b0f1bebb7781da64c298b
Diego Quan <dquan@delicatessen502.com>
2026-05-21 22:10:16 -0600

    Nuevos emojis

--- frontend/src/utils/categorias.js
+++ frontend/src/utils/categorias.js
@@ -1,7 +1,7 @@
 export const categorias = [
-    { id: 'grupo1', nombre : 'Grupo K', emoji : 'K', color : '#E63946' },
-    { id: 'grupo2', nombre : 'Grupo L', emoji : 'L', color : '#457B9D' },
+    { id: 'grupo1', nombre : 'Grupo K', emoji : '1️⃣', color : '#E63946' },
+    { id: 'grupo2', nombre : 'Grupo L', emoji : '2️⃣', color : '#457B9D' },
     { id: 'estrellas', nombre : 'Estrellas', emoji : '⭐', color : '#F4A261' },
     { id: 'especiales', nombre : 'Especiales', emoji : '✨', color : '#2A9D8F' },
-    { id: 'grupo3', nombre : 'Grupo J', emoji : 'J', color : '#54b872' },
+    { id: 'grupo3', nombre : 'Grupo M', emoji : '3️⃣', color : '#54b872' },
 ]

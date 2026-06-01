55785d49870a63272efa01737288b221cbbcbe1d
Diego Quan <dquan@delicatessen502.com>
2026-05-31 18:44:24 -0600

    Vercel

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3001
 
 app.use(cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:5173'
-}))
+})) //Vercel
 app.use(express.json())
 app.use('/api/items', itemsRouter)
 
